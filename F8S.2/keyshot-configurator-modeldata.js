72c00ba0f08bdd0ec93054f3c628b3f4dd682c6b
mikey45097 <91089553+mikey45097@users.noreply.github.com>
2022-09-29 01:25:42 +0530

    f8spider test1

---
+++ F8S.2/keyshot-configurator-modeldata.js
@@ -0,0 +1,67 @@
+model =
+{
+    "materialgroups": [
+        {
+            "materials": [
+                {
+                    "checked": true,
+                    "icon": "stripe%201.png",
+                    "multimaterials": [
+                        "1304+1391",
+                        "1377+1467"
+                    ],
+                    "name": "Stripe 1"
+                },
+                {
+                    "icon": "stripe%202.png",
+                    "multimaterials": [
+                        "1304+1390",
+                        "1377+1431"
+                    ],
+                    "name": "Stripe 2"
+                },
+                {
+                    "icon": "stripe%203.png",
+                    "multimaterials": [
+                        "1304+1398",
+                        "1377+1542"
+                    ],
+                    "name": "Stripe 3"
+                },
+                {
+                    "icon": "stripe%204.png",
+                    "multimaterials": [
+                        "1304+1406",
+                        "1377+1549"
+                    ],
+                    "name": "Stripe 4"
+                }
+            ],
+            "name": "Material set 1"
+        }
+    ],
+    "products": [
+        {
+            "checked": true,
+            "icon": "default.png",
+            "materialgroups": [
+                "Material set 1"
+            ],
+            "modelset": 1005,
+            "name": "Default"
+        }
+    ],
+    "studios": [
+        {
+            "checked": true,
+            "icon": "front.png",
+            "id": 985360900,
+            "name": "Front"
+        },
+        {
+            "icon": "rear.png",
+            "id": 985360901,
+            "name": "Rear"
+        }
+    ]
+};
